3653b139d54462369da7b13460a2a2988ef1bd17
Siyanda-Mzam <dot.netntoz@google.com>
2016-11-12 14:00:52 +0200

    Added minimal styling for element positioning. Also introduced views for signing in and up.

--- app/login/login.component.ts
+++ app/login/login.component.ts
@@ -2,9 +2,21 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'login',
-    template: require('./login.component.html')
+    template: require('./login.component.html'),
+    styles: [require('../signup/signup.component.css')]
 })
 export class SigninComponent {
-    title: string = 'Check what have you been missing';
-    body: string = 'Find out who has shown interest in your craft'; 
+    title: string = 'Check what you have been missing';
+    body: string = 'Find out who has shown interest in your craft';
+    head: string = 'Remember';
+    summary: string = "If you're automatically sure that you know \
+    what reality is and who and what is really important, if you \
+    want to operate on your default setting, then you, like me, \
+    probably won't consider possibilities that aren't annoying and \
+    miserable. But if you've really learned how to think, how to pay \
+    attention, then you'll know you have other options. It will actually \
+    be within your power to experience a crowded, hot, slow, consumer \
+    hell-type situation as not only meaningful, but sacred -- on fire \
+    with the same force that lit the stars: love, fellowship, the mystical \
+    oneness of all things deep down.";
 }
--- app/signup/signup.component.ts
+++ app/signup/signup.component.ts
@@ -2,9 +2,19 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'signup',
-    template: require('./signup.component.html')
+    template: require('./signup.component.html'),
+    styles: [require('./signup.component.css')]
 })
 export class SignupComponent {
     title: string = 'Let\'s get this career to the next level';
-    body: string = 'Sign up today and stand a chance to be invested on';
+    body: string = 'Let\'s take things up a notch. Join us!';
+    summary: string = 'is a free online platform built to serve\
+    as a common ground of reach between musicians and talent seekers.\
+    Artists put up their music on the platform for showcasing to Scouters,\
+    and Scouters in return browse, discover and invest in them.\
+    Join Musinvest to accelerate your music career. \
+    As a scouter, discover talent today and expand your company.';
+    signupSubHeader: string = 'Put your 2 cents in and get a buck back:';
+    confMsg: string = 'You will receive an email to confirm your part in\
+     this venture :)';
 }
