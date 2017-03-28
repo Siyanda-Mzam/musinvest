07c1d252a66b6712a564038494d189754e10c3a0
Siyanda Mzam <siyanda.mzam@hellocomputer.com>
2017-03-28 15:30:17 +0200

    Fixed the non-functional relative paths

--- client/src/tracklist/tracklist.service.ts
+++ client/src/tracklist/tracklist.service.ts
@@ -5,19 +5,19 @@ import {Image} from './tracklist.interface';
 export class TrackListService {
     //IMAGES array implementing Image interface
     images = [
-	{ "title": "The Rapper", "url": "/images/fire-rose.jpg" },
-	{ "title": "Future", "url": "../images/future-pluto.jpg" },
-    { "title": "Tory", "url": "../images/tory-lanez.jpg" },
-	{ "title": "Purple Reign", "url": "../images/esco-terrestrial.png" },
-	{ "title": "Hendrix", "url": "../images/future-magic.jpg" },
-    { "title": "Hago", "url": "../images/future-honest.jpg" },
-    { "title": "Desiigner", "url": "../images/life-of-desiigner.png" },
-    { "title": "Lamar Cover", "url": "../images/lamar-cover.jpg" },
-    { "title": "Drake Thank", "url": "../images/thank-me-later.jpg" },
-    { "title": "Fifty", "url": "../images/no-romeo-no-juliet.jpg" },
-    { "title": "Meek Mill", "url": "../images/meek-mill.jpg" },
-    { "title": "Left Field", "url": "../images/leftfield-lp.png" },
-    { "title": "Youth", "url": "../images/youth.jpg" }
+	{ "title": "The Rapper", "url": "src/images/fire-rose.jpg" },
+	{ "title": "Future", "url": "src/images/future-pluto.jpg" },
+    { "title": "Tory", "url": "src/images/tory-lanez.jpg" },
+	{ "title": "Purple Reign", "url": "src/images/esco-terrestrial.png" },
+	{ "title": "Hendrix", "url": "src/images/future-magic.jpg" },
+    { "title": "Hago", "url": "src/images/future-honest.jpg" },
+    { "title": "Desiigner", "url": "src/images/life-of-desiigner.png" },
+    { "title": "Lamar Cover", "url": "src/images/lamar-cover.jpg" },
+    { "title": "Drake Thank", "url": "src/images/thank-me-later.jpg" },
+    { "title": "Fifty", "url": "src/images/no-romeo-no-juliet.jpg" },
+    { "title": "Meek Mill", "url": "src/images/meek-mill.jpg" },
+    { "title": "Left Field", "url": "src/images/leftfield-lp.png" },
+    { "title": "Youth", "url": "src/images/youth.jpg" }
   ];
   /* Throw*/
   getImages(){
